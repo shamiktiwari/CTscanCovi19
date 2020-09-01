663436415933b35603c680aa2495ed45520f0a17
shamiktiwari <69010383+shamiktiwari@users.noreply.github.com>
2020-09-01 21:58:19 +0530

    Add files via upload

---
+++ CovidCTscan3DCNN.py
@@ -0,0 +1,247 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 20 21:55:16 2020
+
+@author: shamik.tiwari
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import cv2
+import os
+from tqdm import tqdm
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from keras.utils.np_utils import to_categorical
+from keras.models import Model,Sequential, Input, load_model
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D, BatchNormalization, AveragePooling2D, GlobalAveragePooling2D
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from keras.applications import DenseNet121
+disease_types=['COVID', 'non-COVID']
+data_dir = 'E:\covidCTscan'
+train_dir = os.path.join(data_dir)
+train_data = []
+for defects_id, sp in enumerate(disease_types):
+    for file in os.listdir(os.path.join(train_dir, sp)):
+        train_data.append(['{}\{}'.format(sp, file), defects_id, sp])
+        
+train = pd.DataFrame(train_data, columns=['File', 'DiseaseID','Disease Type'])
+train.head()
+
+SEED = 42
+train = train.sample(frac=1, random_state=SEED) 
+train.index = np.arange(len(train)) # Reset indices
+train.head()
+
+plt.hist(train['DiseaseID'])
+plt.title('Frequency Histogram of Species')
+plt.figure(figsize=(12, 12))
+plt.show()
+
+def plot_defects(defect_types, rows, cols):
+    fig, ax = plt.subplots(rows, cols, figsize=(12, 12))
+    defect_files = train['File'][train['Disease Type'] == defect_types].values
+    n = 0
+    for i in range(rows):
+        for j in range(cols):
+            image_path = os.path.join(data_dir, defect_files[n])
+            ax[i, j].set_xticks([])
+            ax[i, j].set_yticks([])
+            ax[i, j].imshow(cv2.imread(image_path))
+            n += 1
+# Displays first n images of class from training set
+plot_defects('COVID', 5, 5)
+
+def plot_defects(defect_types, rows, cols):
+    fig, ax = plt.subplots(rows, cols, figsize=(12, 12))
+    defect_files = train['File'][train['Disease Type'] == defect_types].values
+    n = 0
+    for i in range(rows):
+        for j in range(cols):
+            image_path = os.path.join(data_dir, defect_files[n])
+            ax[i, j].set_xticks([])
+            ax[i, j].set_yticks([])
+            ax[i, j].imshow(cv2.imread(image_path))
+            n += 1
+# Displays first n images of class from training set
+plot_defects('non-COVID', 5, 5)
+
+IMAGE_SIZE = 128
+def read_image(filepath):
+    return cv2.imread(os.path.join(data_dir, filepath)) # Loading a color image is the default flag
+# Resize image to target size
+def resize_image(image, image_size):
+    return cv2.resize(image.copy(), image_size, interpolation=cv2.INTER_AREA)
+
+X_train = np.zeros((train.shape[0], IMAGE_SIZE, IMAGE_SIZE, 3))
+for i, file in tqdm(enumerate(train['File'].values)):
+    image = read_image(file)
+    if image is not None:
+        X_train[i] = resize_image(image, (IMAGE_SIZE, IMAGE_SIZE))
+# Normalize the data
+X_Train = X_train / 255.
+print('Train Shape: {}'.format(X_Train.shape))
+del X_train
+
+Y_train = train['DiseaseID'].values
+Y_train = to_categorical(Y_train, num_classes=2)
+
+BATCH_SIZE = 64
+
+# Split the train and validation sets 
+X_train, X_1, y_train, y_1 = train_test_split(X_Train, Y_train, test_size=0.3, random_state=27)
+
+#Train Test Split
+X_cv, X_test, y_cv, y_test = train_test_split(X_1, y_1, test_size=0.3, random_state=33)
+
+X_Train = X_Train.reshape(X_Train.shape[0], 32, 32, 16, 3)
+X_train = X_train.reshape(X_train.shape[0], 32, 32, 16, 3)
+X_test = X_test.reshape(X_test.shape[0], 32, 32, 16, 3)
+X_cv = X_cv.reshape(X_cv.shape[0], 32, 32, 16, 3)
+
+
+from keras.layers import Conv3D, MaxPool3D, Flatten, Dense
+from keras.layers import Dropout, Input, BatchNormalization
+from sklearn.metrics import confusion_matrix, accuracy_score
+from plotly.offline import iplot, init_notebook_mode
+from keras.losses import categorical_crossentropy
+from keras.optimizers import Adadelta
+import plotly.graph_objs as go
+from matplotlib.pyplot import cm
+from keras.models import Model
+import numpy as np
+import keras
+import h5py
+
+init_notebook_mode(connected=True)
+input_layer = Input((32,32, 16, 3))
+
+## convolutional layers
+conv_layer1 = Conv3D(filters=8, kernel_size=(3, 3, 3), activation='relu')(input_layer)
+conv_layer2 = Conv3D(filters=16, kernel_size=(3, 3, 3), activation='relu')(conv_layer1)
+
+## add max pooling to obtain the most imformatic features
+pooling_layer1 = MaxPool3D(pool_size=(2, 2, 2))(conv_layer2)
+
+conv_layer3 = Conv3D(filters=32, kernel_size=(3, 3, 3), activation='relu')(pooling_layer1)
+conv_layer4 = Conv3D(filters=64, kernel_size=(3, 3, 3), activation='relu')(conv_layer3)
+pooling_layer2 = MaxPool3D(pool_size=(2, 2, 2))(conv_layer4)
+
+## perform batch normalization on the convolution outputs before feeding it to MLP architecture
+pooling_layer2 = BatchNormalization()(pooling_layer2)
+flatten_layer = Flatten()(pooling_layer2)
+
+## create an MLP architecture with dense layers : 4096 -> 512 -> 10
+## add dropouts to avoid overfitting / perform regularization
+dense_layer1 = Dense(units=2048, activation='relu')(flatten_layer)
+dense_layer1 = Dropout(0.4)(dense_layer1)
+dense_layer2 = Dense(units=512, activation='relu')(dense_layer1)
+dense_layer2 = Dropout(0.4)(dense_layer2)
+dense_layer3 = Dense(units=512, activation='relu')(dense_layer2)
+dense_layer3 = Dropout(0.4)(dense_layer3)
+output_layer = Dense(units=2, activation='softmax')(dense_layer2)
+
+## define the model with input layer and output layer
+model = Model(inputs=input_layer, outputs=output_layer)
+
+optimizer = Adam(lr=0.0015, beta_1=0.9, beta_2=0.999, epsilon=0.1, decay=0.0)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+hist=model.fit(x=X_train, y=y_train, batch_size=128, epochs=100, validation_data=[X_cv,y_cv])
+                        
+
+y_pred= model.predict(X_Train)
+predicted_classes = np.argmax(np.round(y_pred),axis=1)
+tar = np.argmax(np.round(Y_train),axis=1)
+from sklearn.metrics import classification_report
+target_names = ["Class {}".format(i) for i in range(2)]
+print(classification_report(tar, predicted_classes, labels=None, target_names=None))
+
+
+from sklearn.metrics import roc_curve,auc
+from scipy import interp
+from itertools import cycle
+n_classes=2
+lw=2
+y_score = y_pred
+target=Y_train
+### MACRO
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+for i in range(n_classes):
+    fpr[i], tpr[i], _ = roc_curve(target[:, i], y_score[:, i])
+    roc_auc[i] = auc(fpr[i], tpr[i])
+
+# Compute micro-average ROC curve and ROC area
+fpr["micro"], tpr["micro"], _ = roc_curve(target.ravel(), y_score.ravel())
+roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+# Plot ROC curves for the multiclass problem
+
+# Compute macro-average ROC curve and ROC area
+
+# First aggregate all false positive rates
+all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+# Then interpolate all ROC curves at this points
+mean_tpr = np.zeros_like(all_fpr)
+for i in range(n_classes):
+    mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+
+# Finally average it and compute AUC
+mean_tpr /= n_classes
+
+fpr["macro"] = all_fpr
+tpr["macro"] = mean_tpr
+roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+# Plot all ROC curves
+plt.figure()
+plt.plot(fpr["micro"], tpr["micro"],
+         label='micro-average ROC curve (area = {0:0.2f})'
+               ''.format(roc_auc["micro"]),
+         color='deeppink', linestyle=':', linewidth=4)
+
+plt.plot(fpr["macro"], tpr["macro"],
+         label='macro-average ROC curve (area = {0:0.2f})'
+               ''.format(roc_auc["macro"]),
+         color='navy', linestyle=':', linewidth=4)
+
+colors = cycle(['aqua', 'darkorange', 'cornflowerblue','red'])
+for i, color in zip(range(n_classes), colors):
+    plt.plot(fpr[i], tpr[i], color=color, lw=lw,
+             label='ROC curve of class {0} (area = {1:0.2f})'
+             ''.format(i, roc_auc[i]))
+
+plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic curve for Covid-19 detection model')
+plt.legend(loc="lower right")
+plt.grid(True)
+plt.show()
+
+y_pred = np.argmax(y_pred, axis=1)
+Y_train = np.argmax(Y_train, axis=1)
+cm = confusion_matrix(Y_train, y_pred)
+plt.figure(figsize=(8, 8))
+ax = sns.heatmap(cm, cmap=plt.cm.YlGn, annot=True, square=True, xticklabels=disease_types, yticklabels=disease_types,annot_kws={"fontsize":14})
+ax.set_ylabel('Actual', fontsize=10)
+ax.set_xlabel('Predicted', fontsize=10)
+
+
+
+import tensorflow as tf
+from keras.utils.vis_utils import plot_model
+tf.keras.utils.plot_model(
+    model,
+    to_file="model.png",
+    show_shapes=False,
+    show_layer_names=True,
+    rankdir="TB",
+    )
